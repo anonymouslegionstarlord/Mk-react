91036397c643b96bfb0ee5502b3124518e7b4987
anonymouslegionstarlord <radheyshyamdubey51@gmail.com>
2024-10-04 18:34:50 +0530

    React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Chai aur React | Installion and working with React and Vite</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+function App() {
+ 
+
+  return (
+    <h1>Chai aur React with Vite | Mayank
+    </h1>
+  )
+}
+
+export default App
